4993c34354dc4867f6adf75e631d200f84872f67
By Speece <72341542+by-speece@users.noreply.github.com>
2021-01-09 23:33:22 +0100

    added yay

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,3 +4,10 @@ git pull
 cp -rf ~/.tools-customconfigs/backup.sh ~/.tools/settings/backup.sh
 sudo cp -rf ~/.tools/.install/tools  /usr/bin/
 sh tools
+cd ~
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+cd ~
+rm -rf yay-bin
+sh tools
