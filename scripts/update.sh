752f678b73d50984087fbaa5da57133fe8df1805
By Speece <szymon.galka.pilne@gmail.com>
2021-01-09 16:24:17 +0100

    added script for install .tools

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,3 +2,4 @@ mkdir ~/.tools-customconfigs
 cp -rf ~/.tools/settings/backup.sh ~/.tools-customconfigs
 git pull
 cp -rf ~/.tools-customconfigs/backup.sh ~/.tools/settings/backup.sh
+sudo cp -rf ~/.tools/.install/tools  /usr/bin/
