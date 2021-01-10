43ee40a91b77c8661f88a1596411c85616633eaf
By Speece <72341542+by-speece@users.noreply.github.com>
2021-01-10 17:52:47 +0100

    some changes

--- .install/install.sh
+++ .install/install.sh
@@ -1,11 +1,16 @@
 cd ~
 git clone https://github.com/by-speece/.tools
-sudo pacman -Syu --needed --noconfirm whiptail reflector
+sudo pacman -Syu --needed --noconfirm reflector
 cd ~/.tools
 git submodule update --init --recursive --remote
 cd ~/.tools/ARPM
 git pull
 sudo cp -rf ~/.tools/.install/tools /usr/bin/
 cd ~
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+cd ~
+rm -rf yay-bin
 rm install.sh
 tools
