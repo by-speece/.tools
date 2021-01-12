584e49c93326b7969e9f299470789dcac6daa64d
By Speece <72341542+by-speece@users.noreply.github.com>
2021-01-12 20:07:29 +0100

    Update update.sh

--- .install/update.sh
+++ .install/update.sh
@@ -4,7 +4,8 @@ mkdir ~/.tools.tmp
 cp -rf ~/.tools.d/i3-autorun/config.d/config.base   ~/.tools.tmp
 cp -rf ~/.tools.d/i3-autorun/config.d/i3            ~/.tools.tmp
 rm -rf ~/.tools.d/
-cp -rf ~/.tools/tools.d  ~/
+mkdir ~/.tools.d
+cp -rf ~/.tools/tools.d/.  ~/.tools.d/
 cp -rf ~/.tools.tmp/config.base   ~/.tools.d/i3-autorun/config.d/config.base
 cp -rf ~/.tools.tmp/i3            ~/.tools.d/i3-autorun/config.d/
 rm -rf ~/.tools.tmp
